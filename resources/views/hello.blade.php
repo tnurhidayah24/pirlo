b9040f320635cebff0f8b09438e15f0d4091d62b
Taufik nurhidayah <tnurhidayah24@gmail.com>
2022-12-13 18:14:23 +0700

    Install Box Icon

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -9,13 +9,17 @@
 </head>
 <body>
     @include('inc.navbar')
-    <div class="hero min-h-screen" style="background-image: url({!! asset('assets/img/hello-pirlo.jpg') !!});">
+    <div class="hero min-h-screen" style="background-image: url({{ asset('assets/img/hello-pirlo.jpg') }});">
         <div class="hero-overlay bg-opacity-60"></div>
         <div class="hero-content text-center text-neutral-content">
             <div class=" max-w-md">
             <h1 class="mb-5 text-5xl font-bold">Andrea Pirlo</h1>
             <p class="mb-5">I'm Coach</p>
-            <button class="btn btn-primary">Get Started</button>
+            <a href=""><i class='bx bxl-twitter mx-1'></i></a>
+            <a href=""><i class='bx bxl-facebook mx-1' ></i></a>
+            <a href=""><i class='bx bxl-instagram mx-1' ></i></a>
+            <a href=""><i class='bx bxl-linkedin mx-1' ></i></a>
+            <a href=""><i class='bx bxl-skype mx-1' ></i></a>
             </div>
         </div>
     </div>
--- resources/views/inc/assets.blade.php
+++ resources/views/inc/assets.blade.php
@@ -1,1 +1,2 @@
 @vite('resources/css/app.css')
+@vite('/node_modules/boxicons/css/boxicons.min.css')
