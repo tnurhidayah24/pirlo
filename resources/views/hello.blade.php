81beccb3608df06759fca021f31c17eacc6507a0
Taufik nurhidayah <tnurhidayah24@gmail.com>
2022-12-13 17:17:46 +0700

    Install DaisyUI, Make a navBar and Hero

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @include('inc.assets')
+</head>
+<body>
+    @include('inc.navbar')
+    <div class="hero min-h-screen" style="background-image: url({!! asset('assets/img/hello-pirlo.jpg') !!});">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content text-center text-neutral-content">
+            <div class=" max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">Andrea Pirlo</h1>
+            <p class="mb-5">I'm Coach</p>
+            <button class="btn btn-primary">Get Started</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
