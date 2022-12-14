fee23cf06f311b2c416b0e997fdbfce5ed364b36
Taufik nurhidayah <tnurhidayah24@gmail.com>
2022-12-14 11:21:13 +0700

    Change Hero

--- resources/views/hello.blade.php
+++ resources/views/hello.blade.php
@@ -9,9 +9,9 @@
 </head>
 <body>
     @include('inc.navbar')
-    <div class="hero min-h-screen" style="background-image: url({{ asset('assets/img/hello-pirlo.jpg') }});">
+    <section class="hero min-h-screen" style="justify-items:unset; background-image: url({{ asset('assets/img/hello-pirlo.jpg') }});">
         <div class="hero-overlay bg-opacity-60"></div>
-        <div class="hero-content text-center text-neutral-content">
+        <div class="hero-content text-center text-neutral-content lg:justify-start lg:text-left lg:pl-72">
             <div class=" max-w-md">
             <h1 class="mb-5 text-5xl font-bold">Andrea Pirlo</h1>
             <p class="mb-5">I'm Coach</p>
@@ -22,7 +22,7 @@
             <a href=""><i class='bx bxl-skype mx-1' ></i></a>
             </div>
         </div>
-    </div>
+    </section>
     @include('inc.footer')
 </body>
 </html>
