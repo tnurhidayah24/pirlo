b1aee4c778c008753eb852af9725f8c361e95272
Taufik nurhidayah <tnurhidayah24@gmail.com>
2022-12-14 17:31:47 +0700

    Install jQuery and Owl.js

--- resources/views/inc/assets.blade.php
+++ resources/views/inc/assets.blade.php
@@ -1,2 +1,6 @@
-@vite('resources/css/app.css')
+@vite(['resources/css/app.css', 'resources/js/app.js'])
 @vite('/node_modules/boxicons/css/boxicons.min.css')
+@vite('/node_modules/owl.carousel/dist/assets/owl.carousel.min.css')
+@vite('/node_modules/owl.carousel/dist/owl.carousel.min.js')
+
+<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
